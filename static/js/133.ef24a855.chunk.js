bcb74ed90ec3e9a8bd7c0722204f6183b98bd912
sergeyakhmad <95075231+sergeyakhmad@users.noreply.github.com>
2022-05-22 21:30:08 +0000

    Deploying to gh-pages from @ sergeyakhmad/goit-react-hw-08-phonebook@0f9b890d425138ece9183d10598dc81161611a4a 🚀

---
+++ static/js/133.ef24a855.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[133],{2133:function(e,t,n){n.r(t);var o=n(184);t.default=function(){return(0,o.jsx)("h1",{style:{display:"flex",justifyContent:"center",marginTop:"20px"},children:"Welcome to the phonebook application!"})}}}]);
+//# sourceMappingURL=133.ef24a855.chunk.js.map
